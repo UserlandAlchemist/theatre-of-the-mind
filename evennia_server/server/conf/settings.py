015e2a6f1a0a123fbacac195016e581deee36a71
Userland Alchemist <alchemist@userlandlab.org>
2025-08-19 04:36:13 +0100

    config(auth): switch to menu_login and update session/account settings

--- evennia_server/server/conf/settings.py
+++ evennia_server/server/conf/settings.py
@@ -33,7 +33,63 @@ from evennia.settings_default import *
 
 # Core identity
 SERVERNAME = "Theatre of the Mind"
+# Short one-sentence blurb describing your game. Shown under the title
+# on the website and could be used in online listings of your game etc.
 GAME_SLOGAN = "Classic adventures, remixed."
+
+# Controls whether new account registration is available.
+# Set to False to lock down the registration page and the create account command.
+NEW_ACCOUNT_REGISTRATION_ENABLED = False
+# Activate telnet service
+TELNET_ENABLED = False
+
+######################################################################
+# Evennia pluggable modules
+######################################################################
+
+# Use the contrib’s connection-screen texts to drive the menu-based login.
+# (You can later point this to your own module if you want a custom banner.)
+CONNECTION_SCREEN_MODULE = "evennia.contrib.base_systems.menu_login.connection_screens"
+
+######################################################################
+# Default command sets and commands
+######################################################################
+
+# Use the menu-based login flow for sessions before an account has logged in.
+# This replaces the default unlogged-in cmdset with the contrib’s version.
+CMDSET_UNLOGGEDIN = "evennia.contrib.base_systems.menu_login.UnloggedinCmdSet"
+
+######################################################################
+# Default Account setup and access
+######################################################################
+
+# Different Multisession modes allow a player (=account) to connect to the
+# game simultaneously with multiple clients (=sessions).
+#  0 - single session per account (if reconnecting, disconnect old session)
+#  1 - multiple sessions per account, all sessions share output
+#  2 - multiple sessions per account, one session allowed per puppet
+#  3 - multiple sessions per account, multiple sessions per puppet (share output)
+#      session getting the same data.
+MULTISESSION_MODE = 2
+# Whether we should create a character with the same name as the account when
+# a new account is created. Together with AUTO_PUPPET_ON_LOGIN, this mimics
+# a legacy MUD, where there is no difference between account and character.
+AUTO_CREATE_CHARACTER_WITH_ACCOUNT = False
+# Whether an account should auto-puppet the last puppeted puppet when logging in. This
+# will only work if the session/puppet combination can be determined (usually
+# MULTISESSION_MODE 0 or 1), otherwise, the player will end up OOC. Use
+# MULTISESSION_MODE=0, AUTO_CREATE_CHARACTER_WITH_ACCOUNT=True and this value to
+# mimic a legacy mud with minimal difference between Account and Character. Disable
+# this and AUTO_PUPPET to get a chargen/character select screen on login.
+AUTO_PUPPET_ON_LOGIN = False
+# How many *different* characters an account can puppet *at the same time*. A value
+# above 1 only makes a difference together with MULTISESSION_MODE > 1.
+MAX_NR_SIMULTANEOUS_PUPPETS = 1
+# The maximum number of characters allowed by be created by the default ooc
+# char-creation command. This can be seen as how big of a 'stable' of characters
+# an account can have (not how many you can puppet at the same time). Set to
+# None for no limit.
+MAX_NR_CHARACTERS = 5
 
 
 ######################################################################
